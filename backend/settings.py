aac11f37e51b613d3fb007cece4b3d97325cbf6e
stevepurpose <stephenodogwu83@gmail.com>
2024-07-18 12:33:49 +0100

    allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['myjobtracker.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['myjobtracker.onrender.com/signup/login', 'myjobtracker.onrender.com/signup/register', 'localhost', '127.0.0.1']
 
 
 # Application definition
